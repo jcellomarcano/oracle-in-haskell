dbdc8edd3ca40151c303eb4a86bbdd5351187007
Jesus Marcano <jcellomarcano@gmail.com>
2021-02-28 23:44:40 -0400

    main

--- Oraculo.hs
+++ Oraculo.hs
@@ -1,21 +1,44 @@
 module Oraculo where
 import qualified Data.Map as Map
+import Data.Maybe ( fromJust )
 
 type Opciones = Map.Map String Oraculo
 
 data Oraculo = Prediccion String
             | Pregunta String Opciones
-            deriving(Show, Read)
+            deriving(Show, Read, Eq)
 
 --Funcion de construcción
 
 crearOraculo :: String -> Oraculo
 crearOraculo = Prediccion
 
--- pregunta :: Oraculo -> String
--- pregunta x = if x == Pregunta then show x else "error" 
+pregunta :: Oraculo -> String
+pregunta (Pregunta preg _) = preg
+pregunta _              = error "No ha ingresado una PREGUNTA"
 
--- chequePregunta :: String -> Oraculo -> Bool
--- chequePregunta str(Pregunta x) =
+prediccion :: Oraculo -> String
+prediccion (Prediccion predic) = predic
+prediccion _              = error "No ha ingresado una PREDICCION"
+
+opciones :: Oraculo -> Opciones
+opciones (Pregunta _ opci) = opci
+opciones _              = error "No ha ingresado una Pregunta "
+
+respuesta :: Oraculo -> String -> Oraculo
+respuesta (Pregunta _ preg) y =
+    case Map.lookup y preg of
+        Just resp -> resp
+        Nothing  -> error "RESPUESTA invalida"
+
+respuesta _ _= error "No ha ungresado una PREGUNTA"
+
+ramificar :: [String] -> [Oraculo] -> String -> Oraculo
+ramificar ops orac preg = Pregunta preg ( Map.fromList ( zip ops orac))
+
+
+
+
+
 
 
