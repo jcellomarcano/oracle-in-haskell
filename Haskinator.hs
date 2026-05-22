93bd236d3cedcbaca567e435d6623df25d8d5916
Jesus Marcano <jcellomarcano@gmail.com>
2026-05-22 22:00:18 +0200

    Integrate Oracle Statistics & Tree visualization into Haskinator menu

--- Haskinator.hs
+++ Haskinator.hs
@@ -3,134 +3,158 @@ import System.Exit ( exitSuccess )
 import System.Directory (doesFileExist)
 import Prelude
 import Data.Map as Map
-import Oraculo
+import Oracle
 import UserInterface
 import Predictions
 
-exit :: IO Oraculo
-exit = do {putStrLn "Gracias por usar Haskinator. Nos vemos pronto!"; exitSuccess }
+-- | Exit the application with a friendly message
+exitApp :: IO Oracle
+exitApp = do
+    putStrLn "Thank you for using Haskinator. See you soon!"
+    exitSuccess
 
-exitINIT :: IO()
-exitINIT = do {putStrLn "Gracias por usar Haskinator. Nos vemos pronto!"; exitSuccess }
+exitInit :: IO ()
+exitInit = do
+    putStrLn "Thank you for using Haskinator. See you soon!"
+    exitSuccess
 
-createOracle :: IO Oraculo
+-- | Prompt the user for an initial prediction and construct a new Oracle
+createOracle :: IO Oracle
 createOracle = do
-        putStrLn "Inserta una predicción"
-        crearOraculo <$> getLine
+    putStrLn "Insert a prediction:"
+    newOracle <$> getLine
 
 main :: IO ()
 main = do
-        putStrLn "***********************"
-        putStrLn "Bienvenido a Haskinator"
-        putStrLn "Selecciona la opción a realizar:"
-        menu Nothing
+    putStrLn "***********************"
+    putStrLn "Welcome to Haskinator"
+    putStrLn "Select an option to perform:"
+    menu Nothing
 
-
-menu :: Maybe Oraculo -> IO ()
+-- | Main interactive menu loop
+menu :: Maybe Oracle -> IO ()
 menu (Just oracle) = do
-        putStrLn . unlines $ Prelude.map concatNums choices
-        choice <- getLine
-        new_oracle <- case validate choice of
-            Just 1 -> createOracle
-            Just 2 -> doPrediction oracle
-            Just 3 -> persistOracle oracle
-            Just 4 -> chargeOracle
-            Just 5 -> crucialQuestion
-            Just 6 -> exit
-            Nothing -> do {putStrLn "Opción inválida"; return oracle}
-        if choice /= "6"
-            then do
-                putStrLn ""
-                menu (Just new_oracle)
-            else
-                putStrLn ""
-   where concatNums (i, s) = show i ++ ": " ++ s
+    putStrLn . unlines $ Prelude.map concatNums choices
+    choice <- getLine
+    newOracleState <- case validate choice of
+        Just 1 -> createOracle
+        Just 2 -> doPrediction oracle
+        Just 3 -> persistOracle oracle
+        Just 4 -> loadOracle
+        Just 5 -> crucialQuestion oracle
+        Just 6 -> showStatsAndTree oracle
+        Just 7 -> exitApp
+        Nothing -> do
+            putStrLn "Invalid option."
+            return oracle
+    if choice /= "7"
+        then do
+            putStrLn ""
+            menu (Just newOracleState)
+        else
+            putStrLn ""
+  where
+    concatNums (i, s) = show i ++ ": " ++ s
+
 menu Nothing = do
-        putStrLn . unlines $ Prelude.map concatNums choices
-        choice <- getLine
-        case validate choice of
-            Just 1  -> do
-                new_instance <- createOracle
-                menu (Just new_instance)
-            Just 4  -> do
-                new_instance <- chargeOracle
-                menu (Just new_instance)
-            Just 6  -> exitINIT
-            Nothing -> do {putStrLn "Opción inválida"; menu Nothing}
-            _ -> do {putStrLn "No hay oráculo cargado"; menu Nothing}
-   where concatNums (i, s) = show i ++ ": " ++ s
+    putStrLn . unlines $ Prelude.map concatNums choices
+    choice <- getLine
+    case validate choice of
+        Just 1  -> do
+            newOracleState <- createOracle
+            menu (Just newOracleState)
+        Just 4  -> do
+            newOracleState <- loadOracle
+            menu (Just newOracleState)
+        Just 7  -> exitInit
+        Nothing -> do
+            putStrLn "Invalid option."
+            menu Nothing
+        _ -> do
+            putStrLn "No oracle loaded."
+            menu Nothing
+  where
+    concatNums (i, s) = show i ++ ": " ++ s
 
-
+-- | Validates if the selected menu option is within range
 validate :: String -> Maybe Int
 validate s = isValid (reads s)
-   where isValid []            = Nothing
-         isValid ((n, _):_)
-               | outOfBounds n = Nothing
-               | otherwise     = Just n
-         outOfBounds n = n < 1 || n > length choices
+  where
+    isValid []            = Nothing
+    isValid ((n, _):_)
+        | outOfBounds n   = Nothing
+        | otherwise       = Just n
+    outOfBounds n         = n < 1 || n > length choices
 
+-- | The list of menu choices in English
 choices :: [(Int, String)]
-choices = zip [1.. ] [
-    "Crear un oráculo nuevo"
-    , "Predecir"
-    , "Persistir"
-    , "Cargar"
-    , "Consultar pregunta crucial"
-    , "Salir"
+choices = zip [1.. ]
+    [ "Create a new oracle"
+    , "Predict"
+    , "Persist"
+    , "Load"
+    , "Consult crucial question"
+    , "Show Oracle Statistics & Tree"
+    , "Exit"
     ]
 
--- execute :: Int -> IO ()
--- execute n = doExec $ filter (\(i, _) -> i == n) choices
---    where doExec ((_, (_,f)):_) = f
-
-persistOracle :: Oraculo -> IO Oraculo
+-- | Saves the current oracle state to a file
+persistOracle :: Oracle -> IO Oracle
 persistOracle oracle = do
-                        putStrLn "Coloca el nombre de archivo en donde se guardará el oráculo: "
-                        filename <- getLine
-                        writeFile filename (show oracle)
-                        return oracle
+    putStrLn "Enter the file name to save the oracle: "
+    filename <- getLine
+    writeFile filename (show oracle)
+    putStrLn "Oracle saved successfully."
+    return oracle
 
+-- | Loads an oracle state from a file
+loadOracle :: IO Oracle
+loadOracle = do
+    putStrLn "Enter the file name of the oracle to load: "
+    filename <- getLine
+    exist <- doesFileExist filename
+    if exist
+        then do
+            str <- readFile filename
+            case readMaybe str of
+                Just oracle -> do
+                    putStrLn "Oracle loaded successfully."
+                    return oracle
+                Nothing -> do
+                    error "Error: Could not parse oracle from file."
+        else do
+            error "Error: File does not exist."
+
+-- | Helper to parse a string representation of a type
 readMaybe :: (Read a) => String -> Maybe a
 readMaybe s = case reads s of
-                [(x, "")] -> Just x
-                _         -> Nothing
+    [(x, "")] -> Just x
+    _         -> Nothing
 
-cargar :: IO Oraculo
-cargar = do
-        putStrLn "Introduzca el archivo a leer."
-        archivo <- getLine
-        s <- readFile archivo
-        return $ oraculo s
-    where
-        oraculo s = read s :: Oraculo
+-- | Prompt for two predictions and display the crucial question distinguishing them
+crucialQuestion :: Oracle -> IO Oracle
+crucialQuestion oracle = do
+    putStrLn "Enter the first prediction:"
+    p1 <- getLine
+    putStrLn "Enter the second prediction:"
+    p2 <- getLine
+    case getCrucialQuestion p1 p2 oracle of
+        Left err -> do
+            putStrLn $ "Error: " ++ err
+        Right (q, o1, o2) -> do
+            putStrLn $ "The crucial question is: \"" ++ q ++ "\""
+            putStrLn $ "For \"" ++ p1 ++ "\", the option is: \"" ++ o1 ++ "\""
+            putStrLn $ "For \"" ++ p2 ++ "\", the option is: \"" ++ o2 ++ "\""
+    return oracle
 
-chargeOracle :: IO Oraculo
-chargeOracle = do
-                putStrLn "Coloca el nombre del archivo en donde se encuentra el oráculo: "
-                filename <- getLine
-                exist <- doesFileExist filename
-                if exist then do
-                        str <- readFile filename
-                        return $ oracle str
-                else
-                    error "El archivo no existe."
-
-                where
-                    oracle str = read str :: Oraculo
-
---------Module Prediction
-
-
--- requestQuestion :: Oraculo -> IO Oraculo
--- requestQuestion = 
-
-
-------------Modulo Interact
-
-crucialQuestion :: IO Oraculo
-crucialQuestion = error  "Función no disponible por los momentos :("
-
-
-
-
-foo = undefined
+-- | Display statistics and pretty ASCII visualization of the oracle
+showStatsAndTree :: Oracle -> IO Oracle
+showStatsAndTree oracle = do
+    putStrLn "=== Oracle Statistics ==="
+    putStrLn $ "Total Questions:   " ++ show (countQuestions oracle)
+    putStrLn $ "Total Predictions: " ++ show (countPredictions oracle)
+    putStrLn $ "Maximum Depth:     " ++ show (maxDepth oracle)
+    putStrLn ""
+    putStrLn "=== Oracle Tree Visualization ==="
+    putStr (prettyPrintOracle oracle)
+    return oracle
